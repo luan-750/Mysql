c15b3cb11bd4e0cee1624739448a5e66b9bef0b2
Luan Oliveira Santos <luanoliveirasantos@hotmail.com>
2020-05-04 03:00:42 -0300

    Finalização da atividade 4 do projeto info x

--- infox.sql
+++ infox.sql
@@ -57,3 +57,30 @@ values('Linus Torvalds','Rua Tux','9999-9999','linus@linux.com');
 
 select * from tbclientes;
 
+use dbinfox;
+create table tbos(
+os int primary key auto_increment,
+data_os timestamp default current_timestamp,
+equipamento varchar(150) not null,
+defeito varchar(150) not null,
+servico varchar(150),
+tecnico varchar(30),
+valor decimal(10,2),
+idcli int not null,
+foreign key(idcli) references tbclientes(idcli)
+);
+
+describe tbos;
+
+insert into tbos (equipamento,defeito,servico,tecnico,valor,idcli)
+values ('Notebook','Não liga','Troca da fonte','Luan',87.50,1);
+
+select * from tbos;
+
+-- O código abaixo traz informações de duas tabelas
+select
+O.os,equipamento,defeito,servico,valor,
+C.nomecli,fonecli
+from tbos as O
+inner join tbclientes as C
+on (O.idcli = C.idcli);
